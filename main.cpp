b40c0fa8c491c3275053b0b821912f066223e6b0
moguchev <abracadabra.14@mail.ru>
2020-03-02 11:04:14 +0200

    logs

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
 int main(int argc, char** argv) {
   logger::init_log(true, false);
   if (argc > 1) {
+    BOOST_LOG_TRIVIAL(info) << "Configure file is in " << argv[1];
     auto conf = ConfigParser::parse(argv[1]);
+    BOOST_LOG_TRIVIAL(info) << "DOCUMENT_ROOT: " << conf.document_root;
+
     server s(conf.host, conf.port, conf.cpu_limit - 1, conf.document_root);
     return s.run();
   } else {
-    BOOST_LOG_TRIVIAL(error) << "expected path to conf in argv";
+    BOOST_LOG_TRIVIAL(error) << "No path to configure file";
     return EXIT_FAILURE;
   }  
 }
